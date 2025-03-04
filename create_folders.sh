ae3ddfcc8aacac76d381af4c7875d230b49260dc
Thabang Mposula <78591599+iarxii@users.noreply.github.com>
2025-03-04 15:42:38 +0200

    Create create_folders.sh
    Creates a DATA folder as well as the logs folder

---
+++ create_folders.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create the DATA and logs directories
+mkdir -p DATA logs
+
+# Create a blank log file inside the logs directory
+touch logs/script_process.log
+
+echo "Directories and log file created successfully."
